f1d81a405c43d68266bc14566dce33fe41166686
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2025-08-16 10:21:55 +0200

    [ADD] account_peppol_send_format_odoo

---
+++ account_peppol_send_format_odoo/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Peppol Send Format Odoo",
+    "summary": """Convert invoices to Peppol XML using the Odoo's account_edi_ubl_cii module.""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/acsone/odoo-peppol-backport",
+    "depends": [
+        "account_edi_ubl_cii",
+        "account_peppol_backport",
+    ],
+    "excludes": [
+        "account_peppol_send_format_oca",
+    ],
+    "data": [],
+    "demo": [],
+}
