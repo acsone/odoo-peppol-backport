84ae5bdd627c23a59cf9e8273ea9b8064ea1e18a
Anton Romanova (roan) <roan@odoo.com>
2025-08-19 14:52:27 +0200

    [PATCH] [FIX] account_peppol: user state desync
    Registered users state never gets fetched from AP API when the user is
    already registered. This is not great when user state somehow gets
    updated by AP server (for whatever reason).
    
    no-task
    
    closes odoo/odoo#223418
    
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- account_peppol_backport/models/account_edi_proxy_user.py
+++ account_peppol_backport/models/account_edi_proxy_user.py
@@ -248,7 +248,7 @@ class AccountEdiProxyClientPeppolUser(models.Model):
             self.env.ref('account_peppol_backport.ir_cron_peppol_get_message_status').method_direct_trigger()
 
     def _cron_peppol_get_participant_status(self):
-        edi_users = self.search([('company_id.account_peppol_proxy_state', 'in', ['pending', 'not_verified', 'sent_verification']), ('proxy_type', '=', 'peppol')])
+        edi_users = self.search([('company_id.account_peppol_proxy_state', '!=', 'not_registered'), ('proxy_type', '=', 'peppol')])
         edi_users._peppol_get_participant_status()
 
     def _peppol_get_participant_status(self):
